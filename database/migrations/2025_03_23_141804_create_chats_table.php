309e77cfd292e4d9caebf44cbf4396939f10e248
JosephDebugger <josephdias012@gmail.com>
2025-03-24 01:41:22 +0600

    database migrated

---
+++ database/migrations/2025_03_23_141804_create_chats_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id()->primary()->autoIncrement();
+            $table->integer('ticket_id')->unique();
+            $table->integer('admin_id');
+            $table->timestamps();
+    
+            $table->foreign('ticket_id')->references('id')->on('tickets');
+            $table->foreign('admin_id')->references('id')->on('users');
+    
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};
